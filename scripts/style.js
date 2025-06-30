53eaaa27c5c139a3444ba399f048c379dd98901f
dani <danimoraga1997@gmail.com>
2025-06-30 15:32:15 +0200

    Tabler: working on it

--- scripts/style.js
+++ scripts/style.js
@@ -1,1 +1,16 @@
 console.log('hello');
+
+function styleDocument() {
+    const screenWidth = screen.width;
+
+    if (screenWidth > 600) {
+        const cards = document.querySelectorAll('.card');
+        console.log(cards);
+        cards.forEach((card) => {
+            const time = card.querySelector('.card__info_bottom__time');
+            time.classList.remove('tp3');
+            time.classList.add('tp1');
+            console.log('hello');
+        })
+    }
+}
