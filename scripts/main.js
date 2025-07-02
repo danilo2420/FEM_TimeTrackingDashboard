c97e662838f9ba56e0eaffc68f8ac420b97b8a57
dani <danimoraga1997@gmail.com>
2025-07-02 11:53:03 +0200

    Styling section done and ready to load data

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,32 @@
-console.log('hello');
+/* Elements */
+const options = document.getElementById('user__options');
 
+/* Variables */
+let currentOption = 'daily';
+
+/* Functions */
+function main() {
+    configureOptions();
+}
+
+function configureOptions() {
+    for (option of options.children) {
+        option.addEventListener('click', (event) => {
+            styleOptions(event);
+            loadData(event.target.dataset.item);
+        })
+    }
+}
+
+function styleOptions(event) {
+    for (option of options.children) {
+        option.classList.remove('item--active');
+    }
+    event.target.classList.add('item--active');
+}
+
+function loadData(value) {
+    console.log('value is ' + value);
+}
+
+main();
